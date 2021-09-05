31fbde86464bf46430c33130832e5cd73ed8f976
ManlyMarco <dummyaccx@gmail.com>
2021-09-05 19:32:00 +0200

    Version up to 6.0.3

--- Core_OverlayMods/Metadata.cs
+++ Core_OverlayMods/Metadata.cs
@@ -2,7 +2,7 @@
 {
     internal static class Metadata
     {
-        public const string Version = "6.0.2";
+        public const string Version = "6.0.3";
         public const string GUID_KSOX = "KSOX";
         public const string GUID_KCOX = "KCOX";
     }
