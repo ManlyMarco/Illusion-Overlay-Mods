a8217890b83a8457f494b093b3706f1e1d5ff231
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2025-02-26 23:21:12 +0100

    Version up to 6.3

--- Core_OverlayMods/Metadata.cs
+++ Core_OverlayMods/Metadata.cs
@@ -2,7 +2,7 @@
 {
     internal static class Metadata
     {
-        public const string Version = "6.2";
+        public const string Version = "6.3";
         public const string GUID_KSOX = "KSOX";
         public const string GUID_KCOX = "KCOX";
     }
