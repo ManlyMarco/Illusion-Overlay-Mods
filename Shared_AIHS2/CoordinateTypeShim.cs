9122efcc63cb4ff719963672c7279c48d56d9c83
ManlyMarco <39247311+ManlyMarco@users.noreply.github.com>
2026-03-11 14:43:00 +0100

    [AI/HS2] Fix clothing overlays failing to load by moving CoordinateType enum
    There might be incompatibilities with other plugins if they reference the CoordinateType enum directly (if they only use the 0 value it should be fine though).

---
+++ Shared_AIHS2/CoordinateTypeShim.cs
@@ -0,0 +1,10 @@
+/// <summary>
+/// Shim required for AI and HS2 since both of these do not have a coord system and this enum is missing.
+/// </summary>
+public enum CoordinateType
+{
+    /// <summary>
+    /// Always use this value.
+    /// </summary>
+    Unknown = 0
+}
